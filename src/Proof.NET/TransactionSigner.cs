935fd320d26abd0ca4493b5ef81b7f9c9895652b
Tre`Von McKay <trevonmckay@users.noreply.github.com>
2025-05-14 22:19:42 -0400

    fix: Signer external Id not returned in CLR object

--- src/Proof.NET/TransactionSigner.cs
+++ src/Proof.NET/TransactionSigner.cs
@@ -1,4 +1,6 @@
-﻿namespace Proof.NET
+﻿using System.Text.Json.Serialization;
+
+namespace Proof.NET
 {
     public class TransactionSigner
     {
@@ -6,14 +8,19 @@
 
         public string? Email { get; set; }
 
+        [JsonPropertyName("first_name")]
         public string? FirstName { get; set; }
 
+        [JsonPropertyName("middle_name")]
         public string? MiddleName { get; set; }
 
+        [JsonPropertyName("last_name")]
         public string? LastName { get; set; }
 
+        [JsonPropertyName("phone_number")]
         public string? PhoneNumber { get; set; }
 
+        [JsonPropertyName("external_id")]
         public string? ExternalId { get; set; }
 
         public string? Entity { get; set; }
@@ -22,8 +29,10 @@
 
         public bool? PersonallyKnownToNotary { get; set; }
 
+        [JsonPropertyName("signing_status")]
         public string? SigningStatus { get; set; }
 
+        [JsonPropertyName("link_expired")]
         public bool? LinkExpired { get; set; }
 
         public string? TransactionAccessLink { get; set; }
