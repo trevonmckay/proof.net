8c92f3576801a5c65f36b9136c4613dd3e663f00
Tre`Von McKay <trevon@hireground.io>
2025-07-10 10:45:14 -0400

    Add DefaultDocumentUrlVersion and Environment options
    Introduces DefaultDocumentUrlVersion and ProofEnvironment properties to ProofClientOptions, with default values set to v2 and Production respectively. Also adds the ProofEnvironment enum to support different environments.

--- src/Proof.NET/ProofClientOptions.cs
+++ src/Proof.NET/ProofClientOptions.cs
@@ -5,11 +5,21 @@
         public string? ApiKey { get; set; }
 
         public string? Endpoint { get; set; }
+
+        public DocumentUrlVersion DefaultDocumentUrlVersion { get; init; } = DocumentUrlVersion.v2;
+
+        public ProofEnvironment Environment { get; init; } = ProofEnvironment.Production;
     }
 
     public enum DocumentUrlVersion
     {
         v1,
         v2
+    }
+
+    public enum ProofEnvironment
+    {
+        Production,
+        Sandbox,
     }
 }
